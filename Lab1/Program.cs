85ffc2c5474546f07619247d935f9d9402a70af4
Alexandra <alexandra.imereli@gmail.com>
2016-07-13 22:26:32 +0300

    Lab1 done

---
+++ Lab1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1 = 99;
+            double number2 = 0.3525;
+            int finalNum1 = (int)(number1 * number2);
+            double finalNum2 = number1 * number2;
+            WriteLine(finalNum1);
+            WriteLine(finalNum2);
+            ReadKey();
+        }
+    }
+}
