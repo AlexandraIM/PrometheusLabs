bfd9fc38a5d86ade111eeee6df99d7a45c9ee7c8
Alexandra <alexandra.imereli@gmail.com>
2016-07-19 10:46:47 +0300

    Commented

--- Lab2/Lab2.2/Program.cs
+++ Lab2/Lab2.2/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿///Напишіть програму, яка виведе всі ПАРНІ цілі числа в діапазоні від 2(включно) до 998(включно) у спадному порядку(998 996 ...). 
+///Кожне число виведіть в новому рядку.
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
