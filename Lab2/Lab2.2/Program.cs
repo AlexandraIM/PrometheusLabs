3c266d38c2ea33a97caca5a50964edbfa6cb2a56
Alexandra <alexandra.imereli@gmail.com>
2016-07-19 10:44:52 +0300

    Lab2.2 created and done

---
+++ Lab2/Lab2.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 999; i > 0; i--)
+            {
+                if (i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
