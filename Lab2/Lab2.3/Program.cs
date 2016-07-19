3ccf4e3ff5ed17f8655944796a4a0cf0038a01b2
Alexandra <alexandra.imereli@gmail.com>
2016-07-19 11:43:53 +0300

    Lab 2.3 created and done

---
+++ Lab2/Lab2.3/Program.cs
@@ -0,0 +1,43 @@
+﻿///Напишіть програму, яка виведе всі цілі числа в діапазоні від 0(включно) до 999(включно) сума цифр яких дорівнює 15 
+///(наприклад, 717 має бути виведене, адже 7 + 1 + 7 = 15, 556 - ні, бо 5 + 5 + 6 = 16) у порядку зростання(69 78 87 96 159 ...). 
+///Кожне число виведіть в новому рядку.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                //якщо число більше або дорівнює 100
+                if (i >= 100)
+                {
+                    int hundr = i / 100; //відібрати соті
+                    int dec = (i - hundr * 100) / 10;// відібрати десяті
+                    int num = (i - hundr * 100) % 10;// залишок
+                    if (hundr + dec + num == 15)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+                //якщо число більше 10 але меньше 100
+                if(i >= 10 && i < 100)
+                {
+                    if ((i/10) + (i%10) == 15)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }
+            }
+            
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lab2/Lab2.3/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("Lab2.3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab2.3")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("3d0510c2-1fc9-42d0-85de-a94b0e0b1cd2")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
